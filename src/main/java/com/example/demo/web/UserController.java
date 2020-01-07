8e1c2925692e153a628fa8963e661ed000950c88
wudidada <hackerjiang@gmail.com>
2020-01-07 23:13:37 +0800

    first step

---
+++ src/main/java/com/example/demo/web/UserController.java
@@ -0,0 +1,45 @@
+package com.example.demo.web;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    static Map<Long, User> users = Collections.synchronizedMap(new HashMap<>());
+
+    @GetMapping("/")
+    public List<User> getUserList() {
+        List<User> r = new ArrayList<>(users.values());
+        return r;
+    }
+
+    @PostMapping("/")
+    public String postUser(@RequestBody User user) {
+        users.put(user.getId(), user);
+        return "success";
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return users.get(id);
+    }
+
+    @PutMapping("/{id}")
+    public String putUser(@PathVariable Long id, @RequestBody User user) {
+        User u = users.get(id);
+        u.setName(user.getName());
+        u.setAge(user.getAge());
+        users.put(id, u);
+        return "success";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        users.remove(id);
+        return "success";
+    }
+
+}
